0922c2fe8310963c9b374936135070c428cedd6f
PTNachiketa <108454923+PTNachiketa@users.noreply.github.com>
2023-07-06 13:36:45 +0530

    Delete BackEnd Directory

--- BackEnd/server.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config()
-const express = require('express')
-const app = express()
-app.listen(process.env.PORT,()=>{
-        console.log(`Server Running on port ${process.env.PORT}`)
-    })
